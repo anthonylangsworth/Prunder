b38b2c4db61168f59a25d4cb835aea8ab6902f60
Anthony Langsworth <anthony.langsworth@gmail.com>
2022-08-01 15:56:40 +1000

    Load config from appsettings.json

--- Pruner/Program.cs
+++ Pruner/Program.cs
@@ -1,32 +1,43 @@
 ﻿using CsvHelper;
 using System.Globalization;
 using Pruner;
+using Microsoft.Extensions.Configuration;
 
-IList<DiscordMember> GetDiscordMembers()
+IList<DiscordMember> GetDiscordMembers(string discordMembersFile)
 {
-    using StreamReader streamReader = new StreamReader("discord-members.csv");
-    using CsvReader csvReader = new CsvReader(streamReader, CultureInfo.InvariantCulture);
+    using StreamReader streamReader = new(discordMembersFile);
+    using CsvReader csvReader = new(streamReader, CultureInfo.InvariantCulture);
     csvReader.Context.RegisterClassMap<DiscordMemberMap>();
     return csvReader.GetRecords<DiscordMember>().ToList();
 }
 
-IList<SquadronMember> GetSquadronMembers()
+IList<SquadronMember> GetSquadronMembers(string squadronMembersFile)
 {
-    using StreamReader streamReader = new StreamReader("squadron-members.csv");
-    using CsvReader csvReader = new CsvReader(streamReader, CultureInfo.InvariantCulture);
+    using StreamReader streamReader = new(squadronMembersFile);
+    using CsvReader csvReader = new(streamReader, CultureInfo.InvariantCulture);
     csvReader.Context.RegisterClassMap<SquadronMemberMap>();
     return csvReader.GetRecords<SquadronMember>().ToList();
 }
 
-bool SameCommanderName(string discordMember, string squadronMember)
+bool SameCommanderName(string discordMember, string squadronMember, IReadOnlyDictionary<string, string> discordToSquadronMemberMap)
 {
-
+    return string.Equals(discordMember, squadronMember, StringComparison.InvariantCultureIgnoreCase)
+        || string.Equals(discordMember, "CMDR " + squadronMember, StringComparison.InvariantCultureIgnoreCase)
+        || (discordToSquadronMemberMap.TryGetValue(discordMember, out string? mappedSquadronMember) 
+            && string.Equals(mappedSquadronMember, squadronMember, StringComparison.InvariantCultureIgnoreCase));
 }
 
-IList<DiscordMember> discordMembers = GetDiscordMembers();
-IList<SquadronMember> squadronMembers = GetSquadronMembers();
+IConfiguration config = new ConfigurationBuilder()
+    .AddJsonFile("appsettings.json")
+    .Build();
 
-foreach(string name in squadronMembers.Where(sm => discordMembers.Any(dm => string.Equals(sm.Name, dm.Name, StringComparison.OrdinalIgnoreCase))).Select(sm => sm.Name))
+Dictionary<string, string> discordToSquadronMemberMap = new();
+config.GetRequiredSection("DiscordToSquadronMemberMap").Bind(discordToSquadronMemberMap);
+
+IList<DiscordMember> discordMembers = GetDiscordMembers(config.GetRequiredSection("DiscordMembersFile").Value);
+IList<SquadronMember> squadronMembers = GetSquadronMembers(config.GetRequiredSection("SquadronMembersFile").Value);
+
+foreach(string name in squadronMembers.Where(sm => discordMembers.All(dm => !SameCommanderName(dm.Name, sm.Name, discordToSquadronMemberMap))).Select(sm => sm.Name))
 {
     Console.WriteLine(name);
 }
--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # Pruner
+
+A quick and dirty command-line tool to manage members of an in-game **Elite Dangerous** squadron and the squadron's Discord server.
